217ea1ea6fe7d615f0e2dbb96fb6a9da6fb042d4
Viktor Tytenko <92685344+ViktorTyt@users.noreply.github.com>
2022-05-31 01:27:02 +0300

    add lightbox

--- src/index.js
+++ src/index.js
@@ -15,14 +15,13 @@ refs.loadMore.classList.add('is-hidden');
 const photosApiService = new PhotosApiService();
 
 const render = photos => {
-  console.log(photos.hits[0].webformatURL);
   const photosArr = photos.hits;
-  Notiflix.Notify.success(`Hooray! We found ${photos.totalHits} images.`);
+
   const gallery = photosArr
     .map(
       photo => `<div class="photo-card">
       <a href="${photo.largeImageURL}">
-    <img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy" />
+    <img class="photo-card__image" src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy" />
     </a>
     <div class="info">
       <p class="info-item">
@@ -47,7 +46,14 @@ const render = photos => {
     .join('');
 
   refs.gallery.insertAdjacentHTML('beforeend', gallery);
-  //   console.log(gallery);
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    fadeSpeed: 250,
+    overlayOpacity: 0.7,
+    captionsData: 'alt',
+  });
+
+  lightbox.refresh();
 };
 
 const onSubmit = async event => {
@@ -64,27 +70,18 @@ const onSubmit = async event => {
         'Sorry, there are no images matching your search query. Please try again.',
       );
     }
+
     refs.gallery.innerHTML = '';
-    console.log(getPhotos);
     render(getPhotos);
+
+    Notiflix.Notify.success(`Hooray! We found ${getPhotos.totalHits} images.`);
+
     if (getPhotos.totalHits > photosApiService.perPage) {
       refs.loadMore.classList.remove('is-hidden');
     }
   } catch (error) {
     console.log(error);
   }
-
-  let gallery = new SimpleLightbox('.gallery a', {
-    fadeSpeed: 250,
-    overlayOpacity: 0.7,
-    captionsData: 'alt',
-  });
-
-  gallery.on('show.simplelightbox', function (e) {
-    // do something…
-  });
-
-  gallery.refresh();
 };
 
 const OnLoadMore = async () => {
@@ -92,9 +89,7 @@ const OnLoadMore = async () => {
 
   render(loadMore);
 
-  console.log(loadMore.total);
   const loadedByPage = photosApiService.perPage * photosApiService.page - photosApiService.perPage;
-  console.log(loadedByPage);
 
   if (loadMore.total < loadedByPage) {
     refs.loadMore.classList.add('is-hidden');
